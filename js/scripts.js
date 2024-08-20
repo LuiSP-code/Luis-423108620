4d85c133bb550f2326a4bccb8929a2576aa1981c
luisp <luis03sanches@outlook.com>
2024-08-19 19:08:45 -0600

    Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 let nombre='Luis';
-console.log(`Hola ${nombre}`);
-a=prompt('Como te llamas?')
+let edad= 20
+console.log(`NOMBRE: ${nombre}`);
+console.log(`EDAD: ${nombre}`);
+
+a=prompt('Como te llamas?');
+e=prompt('Cual es tu edad?');
 
 console.log(`Tu nombre es: ${a}`)
+console.log(`Tu nombre es: ${e}`)
