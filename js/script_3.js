c752f8990f0247bf2183a3b255607c47e2831272
luisp <luis03sanches@outlook.com>
2024-08-28 18:14:13 -0600

    TAREA_2

---
+++ js/script_3.js
@@ -0,0 +1,39 @@
+// let a=['mazana','pera','naranja','uva']
+// console.table(a[0])
+// console.log(a.length)
+// console.table(a)
+
+// // FOR
+// for(i=0; i<101;i++){
+//     console.log(i)
+// }
+
+// // FOR IN
+// for (x of a) {
+//     console.log(x)
+// }
+// //FOR ON
+// for (i in a){
+//     console.log(i)
+// }
+// // DECLAR UNA FUNCION 
+// function suma(a,b){    
+//     console.log(a+b);
+// }
+// sumar(5,10);
+
+// function saludo(nombre){
+//     console.log(`Hola comoe estas ${nombre}`)
+// };
+
+// saludo('luis');
+
+// EJECICIO
+
+function sumar(){
+n1= parseInt(prompt('Ingrese el primer numero:'));
+n2= parseInt(prompt('Ingrese el segundo numero:'));
+console.log(`La suma es ${n1+n2}`);
+}
+
+sumar();
